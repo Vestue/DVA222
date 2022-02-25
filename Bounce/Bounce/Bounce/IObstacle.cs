2a4307a0e6130d3d1eb2d97ec0886eeee46a8b4d
Vestue <61783376+Vestue@users.noreply.github.com>
2022-02-25 18:43:19 +0100

    Add IObstacle.cs
    Start working on the bounce assignment.

---
+++ Bounce/Bounce/Bounce/IObstacle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Bounce
+{
+    enum Obstacle { VerticalLine, HorizontalLine, SpeedUpBox, SpeedDownBox }
+    internal interface IObstacle
+    {
+        void UpdateSpeed(PointF Speed);
+        void DrawObject(Graphics g, Obstacle obstacle);
+        void CheckCollision(PointF Position);
+    }
+}
