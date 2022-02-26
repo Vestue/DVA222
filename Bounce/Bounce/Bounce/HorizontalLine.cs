788041cba9b64d0e9dc6cbaae7d7d6e851f7eb96
Vestue <61783376+Vestue@users.noreply.github.com>
2022-02-26 22:33:08 +0100

    Add calculations for horizontal line vectors

--- Bounce/Bounce/Bounce/HorizontalLine.cs
+++ Bounce/Bounce/Bounce/HorizontalLine.cs
@@ -10,8 +10,31 @@ namespace Bounce
     {
         public override bool CheckCollision(PointF ballPosition, float radius)
         {
-            PointF lineVector = PointF(Position.X + Length, Position.Y) - Position;
-            PointF startToBall = Position - ballPosition;
+            PointF startPosition = Position;
+            // End position could possibly be set in Obstacle class to increase code reusability.
+            PointF endPosition = PointF(Position.X + Length, Position.Y);
+
+            // Find which point on the line that is closes to the center of the ball.
+            var lineVector = endPosition - startPosition;
+            var lineNorm = (1 / Length) * lineVector;
+            var startToBall = startPosition - ballPosition;
+            var closestPointOnLine = VectorMath.Dot(startToBall, lineVector) / Length;
+
+            // Check if the closest point is the start or end coordinates
+            PointF closest;
+            if (closestPointOnLine < 0) closest = startPosition;
+            else if (closestPointOnLine > Length) closest = endPosition;
+            else closest = startPosition + closestPointOnLine * lineNorm;
+
+            // Check if the distance from the closest point is less than the radius of the ball.
+            // If this is true, the line is within the ball.
+            var distanceBetweenBallAndClosest = ballPosition - closest;
+
+            if (Math.Sqrt(VectorMath.Dot(distanceBetweenBallAndClosest, distanceBetweenBallAndClosest)) <= radius)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
--- Bounce/Bounce/Bounce/VectorMath.cs
+++ Bounce/Bounce/Bounce/VectorMath.cs
@@ -7,7 +7,7 @@ using System.Drawing;
 
 namespace Bounce
 {
-    class Vector
+    class VectorMath
     {
         public float Dot(float x1, float y1, float x2, float y2) => x1 * x2 + y1 * y2;
         public float Dot(PointF a, PointF b) => a.X * b.X + a.Y * b.Y;
