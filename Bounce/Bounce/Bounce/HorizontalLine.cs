4699b909364a399fd320600bbbb24b05748a9c4a
Vestue <61783376+Vestue@users.noreply.github.com>
2022-02-26 21:27:37 +0100

    Add parts of vector calculation

--- Bounce/Bounce/Bounce/HorizontalLine.cs
+++ Bounce/Bounce/Bounce/HorizontalLine.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace Bounce
 {
-    internal abstract class Box : Obstacle
+    internal abstract class HorizontalLine : Obstacle
     {
         public override bool CheckCollision(PointF ballPosition, float radius)
         {
-
+            PointF lineVector = PointF(Position.X + Length, Position.Y) - Position;
+            PointF startToBall = Position - ballPosition;
         }
     }
 }
---
+++ Bounce/Bounce/Bounce/Vector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Bounce
+{
+    class Vector
+    {
+        public float Dot(float x1, float y1, float x2, float y2) => x1 * x2 + y1 * y2;
+        public float Dot(PointF a, PointF b) => a.X * b.X + a.Y * b.Y;
+    }
+}
