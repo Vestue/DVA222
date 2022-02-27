d5504c469fa7d30cf25e20bc787718e497aa7717
Vestue <61783376+Vestue@users.noreply.github.com>
2022-02-27 11:32:59 +0100

    Update Box.cs
    Add calculations for collision with box.

--- Bounce/Bounce/Bounce/Box.cs
+++ Bounce/Bounce/Bounce/Box.cs
@@ -8,9 +8,26 @@ namespace Bounce
 {
     internal abstract class Box : Obstacle
     {
+        PointF topLeft;
+        PointF topRight;
+        PointF bottomLeft;
+        PointF bottomRight;
+        float Heigth;
         public override bool CheckCollision(PointF ballPosition, float radius)
         {
+            var midX = (topLeft.X + topRight.X) / 2;
+            var midY = (topLeft.Y + bottomLeft.Y) / 2;
 
+            PointF ballDistance = new PointF(Math.Abs(ballPosition.X - midX), Math.Abs(ballPosition.Y - midY));
+
+            if (ballDistance.X > Length / 2 + radius) return false;
+            if (ballDistance.Y > Heigth / 2 + radius) return false;
+
+            if (ballDistance.X <= Length / 2) return true;
+            if (ballDistance.Y <= Heigth / 2) return true;
+
+            var cornerDistance = (ballDistance.X - Length / 2) ^ 2 + (ballDistance.Y - Heigth / 2) ^ 2;
+            return (cornerDistance <= radius ^ 2);
         }
     }
 }
