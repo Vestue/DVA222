a9c5f93995206c08c674f96772310426d0162589
Vestue <61783376+Vestue@users.noreply.github.com>
2022-03-08 18:28:13 +0100

    Initial commit

---
+++ Assignment3_HashDictionary/Assignment3_HashDictionary/HashDictionary.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_HashDictionary
+{
+    internal class HashDictionary : IDictionary<int, string>
+    {
+        public string this[int key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ICollection<int> Keys => throw new NotImplementedException();
+
+        public ICollection<string> Values => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(int key, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(KeyValuePair<int, string> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<int, string> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<int, string>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<int, string>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<int, string> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(int key, [MaybeNullWhen(false)] out string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
