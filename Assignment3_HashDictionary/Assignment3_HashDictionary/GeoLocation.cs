1a62218967ee47a828dad6797b953a7169d1fb45
Vestue <61783376+Vestue@users.noreply.github.com>
2022-03-10 14:24:35 +0100

    Create GeoLocation.cs

---
+++ Assignment3_HashDictionary/Assignment3_HashDictionary/GeoLocation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_HashDictionary
+{
+    internal class GeoLocation
+    {
+        public int _longitude { get; private set; }
+        public int _latitude { get; private set; }
+        public GeoLocation(int longitude, int latitude)
+        {
+            _longitude = longitude;
+            _latitude = latitude;
+        }
+    }
+}
