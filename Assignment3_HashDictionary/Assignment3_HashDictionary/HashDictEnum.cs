9112c9a53eb222dde2fc8560115784ab66facc69
Vestue <61783376+Vestue@users.noreply.github.com>
2022-03-13 18:36:34 +0100

    Update HashDictEnum.cs

--- Assignment3_HashDictionary/Assignment3_HashDictionary/HashDictEnum.cs
+++ Assignment3_HashDictionary/Assignment3_HashDictionary/HashDictEnum.cs
@@ -2,9 +2,10 @@
 
 namespace Assignment3_HashDictionary
 {
-    internal class HashDictEnum : IEnumerator<KeyValuePair<int, int>>
+    internal class HashDictEnum : IEnumerator<KeyValuePair<int, int>>, IEnumerator<KeyValuePair<GeoLocation, string>>
     {
-        List<KeyValuePair<int, int>>[] _htable;
+        List<KeyValuePair<int, int>>[]? _htable;
+        List<KeyValuePair<GeoLocation, string>>[]? _geoTable;
         int _position = -1;
         int _count;
 
@@ -13,6 +14,12 @@ namespace Assignment3_HashDictionary
             _htable = table;
             _count = Length();
         }
+
+        public HashDictEnum(List<KeyValuePair<GeoLocation, string>>[] geoTable)
+        {
+            _count = LengthGeo();
+        }
+        
         public KeyValuePair<int, int> Current
         {
             get
@@ -21,7 +28,6 @@ namespace Assignment3_HashDictionary
                 {
                     throw new InvalidOperationException("Operation not possible");
                 }
-
                 int count = 0;
                 bool runLoop = true;
                 KeyValuePair<int, int> current = new KeyValuePair<int, int>();
@@ -60,10 +66,46 @@ namespace Assignment3_HashDictionary
             _position = -1;
         }
 
+        KeyValuePair<GeoLocation, string> IEnumerator<KeyValuePair<GeoLocation, string>>.Current
+        {
+            get
+            {
+                if (_count == 0)
+                {
+                    throw new InvalidOperationException("Operation not possible");
+                }
+                int count = 0;
+                bool runLoop = true;
+                KeyValuePair<GeoLocation, string> current = new KeyValuePair<GeoLocation, string>();
+                foreach (List<KeyValuePair<GeoLocation, string>> chain in _geoTable)
+                {
+                    foreach (KeyValuePair<GeoLocation, string> pair in chain)
+                    {
+                        if (count == _position)
+                        {
+                            current = pair;
+                            runLoop = false;
+                            break;
+                        }
+                        count++;
+                    }
+                    if (runLoop == false) break;
+                }
+                return current;
+            }
+        }
+
         private int Length()
         {
             int count = 0;
             foreach (List<KeyValuePair<int, int>> chain in _htable)
+                count += chain.Count;
+            return count;
+        }
+        private int LengthGeo()
+        {
+            int count = 0; 
+            foreach (List<KeyValuePair<GeoLocation, string>> chain in _geoTable)
                 count += chain.Count;
             return count;
         }
