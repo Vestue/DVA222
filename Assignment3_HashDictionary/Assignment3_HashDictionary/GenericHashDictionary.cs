ed026510ce98b2bea2f8d5b5f9b4e41cd60c2cb7
Vestue <61783376+Vestue@users.noreply.github.com>
2022-03-16 21:45:34 +0100

    Add ContainsKey()

--- Assignment3_HashDictionary/Assignment3_HashDictionary/GenericHashDictionary.cs
+++ Assignment3_HashDictionary/Assignment3_HashDictionary/GenericHashDictionary.cs
@@ -8,40 +8,55 @@ using System.Threading.Tasks;
 
 namespace Assignment3_HashDictionary
 {
-    internal class GenericHashDictionary<Tkey, TValue> : IDictionary<Tkey, TValue>
+    internal class GenericHashDictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
         private static readonly int _arraySize = 10000;
-        private List<KeyValuePair<Tkey, TValue>>[] _hashTable = new List<KeyValuePair<Tkey, TValue>>[_arraySize];
+        private List<KeyValuePair<TKey, TValue>>[] _hashTable = new List<KeyValuePair<TKey, TValue>>[_arraySize];
         private int _nodeCount;
 
         public GenericHashDictionary()
         {
             for (int i = 0; i < _arraySize; i++)
-                _hashTable[i] = new List<KeyValuePair<Tkey, TValue>>();
+                _hashTable[i] = new List<KeyValuePair<TKey, TValue>>();
         }
 
         // Implementera metoder från föreläsnings-instruktioner
         // private List<KeyValuePair<K, V>> Find(K key), kan returnera null om ingen nyckel hittas
         // uint GetIndex(K key)
 
-        public TValue this[Tkey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private int GetIndex(TKey key) => key.GetHashCode() % _arraySize;
 
-        public ICollection<Tkey> Keys => throw new NotImplementedException();
+        public TValue this[TKey key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ICollection<TKey> Keys => throw new NotImplementedException();
 
         public ICollection<TValue> Values => throw new NotImplementedException();
 
-        public int Count => throw new NotImplementedException();
+        public int Count => _nodeCount;
 
-        public bool IsReadOnly => throw new NotImplementedException();
+        public bool IsReadOnly => false;
 
-        public void Add(Tkey key, TValue value)
+        public void Add(TKey key, TValue value)
         {
             throw new NotImplementedException();
         }
 
-        public void Add(KeyValuePair<Tkey, TValue> item)
+        public void Add(KeyValuePair<TKey, TValue> item)
         {
             throw new NotImplementedException();
+        }
+
+        private KeyValuePair<TKey, TValue> GetKey(TKey key)
+        {
+            if (key == null) return default(KeyValuePair<TKey, TValue>);
+            foreach (KeyValuePair<TKey, TValue> pair in _hashTable[GetIndex(key)])
+                if (key.Equals(pair.Key)) return pair;
+            return default(KeyValuePair<TKey, TValue>);
+        }
+
+        private void SetKey(TKey key)
+        {
+
         }
 
         public void Clear()
@@ -49,37 +64,40 @@ namespace Assignment3_HashDictionary
             throw new NotImplementedException();
         }
 
-        public bool Contains(KeyValuePair<Tkey, TValue> item)
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return _hashTable[GetIndex(item.Key)].Contains(item);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            if (key != null)
+                foreach(KeyValuePair<TKey, TValue> keyValuePair in _hashTable[GetIndex(key)])
+                    if (key.Equals(keyValuePair.Key)) return true;
+            return false;
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
         {
             throw new NotImplementedException();
         }
 
-        public bool ContainsKey(Tkey key)
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
             throw new NotImplementedException();
         }
 
-        public void CopyTo(KeyValuePair<Tkey, TValue>[] array, int arrayIndex)
+        public bool Remove(TKey key)
         {
             throw new NotImplementedException();
         }
 
-        public IEnumerator<KeyValuePair<Tkey, TValue>> GetEnumerator()
+        public bool Remove(KeyValuePair<TKey, TValue> item)
         {
             throw new NotImplementedException();
         }
 
-        public bool Remove(Tkey key)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Remove(KeyValuePair<Tkey, TValue> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool TryGetValue(Tkey key, [MaybeNullWhen(false)] out TValue value)
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
         {
             throw new NotImplementedException();
         }
