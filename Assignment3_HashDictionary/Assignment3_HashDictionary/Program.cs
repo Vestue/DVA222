94da4c87376e297c42c9026d8d5b857be05f954c
Vestue <61783376+Vestue@users.noreply.github.com>
2022-04-10 15:41:07 +0200

    Fix for third attempt

--- Assignment3_HashDictionary/Assignment3_HashDictionary/Program.cs
+++ Assignment3_HashDictionary/Assignment3_HashDictionary/Program.cs
@@ -1,12 +1,12 @@
 ﻿using Assignment3_HashDictionary;
 using HashtableTester;
 
-//IDictionary<int, int> d = new HashDictionary<int, int>();
+IDictionary<int, int> d = new HashDictionary<int, int>();
 //TestDriver.Instance.Run(d, 10000);
 
-IDictionary<GeoLocation, int> test = new HashDictionary<GeoLocation, int>();
-test.Add(new GeoLocation(1, 2), 1);
-var x = test[new GeoLocation(1, 2)];
+var t = new HashDictionary<GeoLocation, int>();
+t.Add(new GeoLocation(1, 2), 1);
+//t.Add(new GeoLocation(1, 2), 1);
+
+var x = t[new GeoLocation(1, 2)];
 Console.WriteLine(x);
-Console.WriteLine((test.ContainsKey(new GeoLocation(1, 2))));
-Console.WriteLine(test.Contains(new KeyValuePair<GeoLocation, int>(new GeoLocation(1, 2), 1)));
